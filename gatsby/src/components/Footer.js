1a2f374bb8a152c48ad7237ad101c48f1dc55821
danleks <lx.dnlvch@gmail.com>
2020-11-08 11:36:26 +0100

    create footer

---
+++ gatsby/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Kwarctex Sp. z o.o.</p>
+        </footer>
+    )
+}
+
+export default Footer
