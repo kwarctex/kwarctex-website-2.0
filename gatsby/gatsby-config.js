b40246e2b107047a88c2180a0dd5ba3f00bfc4b0
danleks <lx.dnlvch@gmail.com>
2020-11-09 14:55:30 +0100

    project updates

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,17 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+// IF this file is empty, some people report that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+module.exports = {
+    siteMetadata: {
+        title: 'Kwarctex website',
+        description: 'Oficjalny przedstawiciel firmy Belenco®',
+    },
+    plugins: [
+        {
+          resolve: "gatsby-plugin-react-svg",
+          options: {
+            rule: {
+              include: /assets/
+            }
+          }
+        }
+      ],
+}
--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -2,10 +2,14 @@ import React from 'react';
 import 'normalize.css';
 import Footer from './Footer'
 import Nav from './Nav'
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 const Layout = ({ children }) => {
     return (
         <div>
+            <GlobalStyles />
+            <Typography />    
             <Nav />
                 { children }
             <Footer />
@@ -13,4 +17,4 @@ const Layout = ({ children }) => {
     )
 }
 
-export default Layout
+export default Layout;
